1eae6adad21c8ddb94410861820a1222eedad8d1
@riskiddin.rustamov <riskiddin.rustamov@paynet.uz>
2024-08-23 15:05:03 +0500

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+
+import withNextra from "nextra";
+
 const nextConfig = {
-  reactStrictMode: true,
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.tsx",
+  latex: true
 };
 
-export default nextConfig;
+export default withNextra(nextConfig);
