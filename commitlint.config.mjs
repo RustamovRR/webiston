6d555fb8ba1093becd880ce7a425eb12c87d82c1
@riskiddin.rustamov <riskiddin.rustamov@paynet.uz>
2024-08-26 14:15:02 +0500

    feat: add linter, husky, commitlint

---
+++ commitlint.config.mjs
@@ -0,0 +1,16 @@
+const config = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test'],
+    ],
+    'type-case': [2, 'always', 'lower-case'],
+    'scope-case': [2, 'always', 'lower-case'],
+    'subject-full-stop': [2, 'never', '.'],
+    'header-max-length': [2, 'always', 72],
+  },
+}
+
+export default config
