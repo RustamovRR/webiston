54c08197559dcabf6cf6a191dedb35463606de6d
@riskiddin.rustamov <riskiddin.rustamov@paynet.uz>
2024-10-07 18:38:59 +0500

    fix: change header-max-length of commitlint

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -9,7 +9,7 @@ const config = {
     'type-case': [2, 'always', 'lower-case'],
     'scope-case': [2, 'always', 'lower-case'],
     'subject-full-stop': [2, 'never', '.'],
-    'header-max-length': [2, 'always', 72],
+    'header-max-length': [2, 'always', 96],
   },
 }
 
