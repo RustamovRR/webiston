5d26dc3736f793e961250bb894efba3eb6e8dc1e
@riskiddin.rustamov <riskiddin.rustamov@paynet.uz>
2024-11-20 17:31:32 +0500

    refactor: review 'common-questions' chapter

--- src/pages/docs/react/common-questions/_meta.js
+++ src/pages/docs/react/common-questions/_meta.js
@@ -1,12 +1,12 @@
 export default {
   'memoization-with-react-memo': 'React.memo bilan memoizatsiya',
-  'getting-fluent-in-react-memo': "React.memo’dan to'liq foydalanish",
+  'getting-fluent-in-react-memo': "React.memo’ni chuqurroq o'rganish",
   'its-guideline-not-rule': "Qoida emas, Yo'riqnoma",
   'memoization-with-useMemo': 'useMemo bilan memoizatsiya',
   'usememo-considered-harmful': "useMemo’dan to'g'ri foydalanish",
   'forget-about-all-this': "Bularni barchasini 'Unuting'",
   'lazy-loading': 'Lazy loading',
-  'greater-control-ui-with-suspense': 'Suspense bilan kengroq UI nazorati',
+  'greater-control-ui-with-suspense': 'Suspense bilan yaxshilangan UI boshqaruvi',
   'usestate-vs-usereducer': 'useState va useReducer',
-  'immer-and-ergonomics': 'Immer va ergonomika',
+  'immer-and-ergonomics': 'Immer va foydalanish qulayligi',
 }
