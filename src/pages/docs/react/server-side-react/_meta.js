92b528161293ca946fd7f7576a125facc479f0f0
@riskiddin.rustamov <riskiddin.rustamov@paynet.uz>
2024-11-22 16:21:35 +0500

    refactor: review 'server-side-react' chapter

--- src/pages/docs/react/server-side-react/_meta.js
+++ src/pages/docs/react/server-side-react/_meta.js
@@ -1,13 +1,13 @@
 export default {
   'limitations-of-client-side-rendering': 'Klient tomonda render qilishning cheklovlari',
-  'rise-of-server-rendering': 'Server renderlashning yuksalishi',
+  'rise-of-server-rendering': 'Serverda renderlashning yuksalishi',
   hydration: 'Hidratsiya',
   'creating-server-rendering': 'Server rendering yaratish',
   'server-rendering-apis-in-react': 'React’da server renderlash API’lari',
   renderToString: 'renderToString API',
   renderToPipeableStream: 'renderToPipeableStream API',
   renderToReadableStream: 'renderToReadableStream API',
-  'when-to-use-what': 'Qaysi API’ni qachon ishlatish kerak',
-  'dont-roll-your-own': "O'zingiz yaratishga urinmang",
+  'when-to-use-what': 'Qachon qaysi API’dan foydalanish kerak',
+  'dont-roll-your-own': "O'zingiz ixtiro qilmang",
   recap: 'Xulosa',
 }
