3c94119af9681b2352f7152a84674f8c4a3ce080
@riskiddin.rustamov <riskiddin.rustamov@paynet.uz>
2024-11-29 16:38:49 +0500

    fix: gtag element

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,28 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
+import { useEffect } from 'react'
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    // Google Analytics initialization
+    const GA_ID = process.env.NEXT_PUBLIC_GA_ID
+
+    const script1 = document.createElement('script')
+    script1.src = `https://www.googletagmanager.com/gtag/js?id=${GA_ID}`
+    script1.async = true
+    document.head.appendChild(script1)
+
+    const script2 = document.createElement('script')
+    script2.innerHTML = `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', '${GA_ID}');
+    `
+    document.head.appendChild(script2)
+  }, [])
+
   return (
     <>
       <Component {...pageProps} />
