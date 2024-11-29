451ba2a442bf7f2df00c03c050d54d9f3c2369e3
@riskiddin.rustamov <riskiddin.rustamov@paynet.uz>
2024-11-29 11:48:10 +0500

    fix: some bugs

---
+++ src/constants/index.ts
@@ -0,0 +1,57 @@
+export const REACT_CHAPTERS = [
+  {
+    href: '/docs/react/entry-level-stuff',
+    title: 'React asoslari',
+    description: "React paydo bo'lgunga qadar bosqich haqida batafsil ma'lumot.",
+  },
+  {
+    href: '/docs/react/jsx',
+    title: 'JSX',
+    description: "JSX qanday ichki ishlashi haqida batafsil ma'lumot.",
+  },
+  {
+    href: '/docs/react/virtual-dom',
+    title: 'Virtual DOM',
+    description: "Virtual DOM qanday ichki ishlashi haqida batafsil ma'lumot.",
+  },
+  {
+    href: '/docs/react/inside-reconciliation',
+    title: 'Reconciliation',
+    description: "Reconciliation mexanizmi qanday ishlashi haqida batafsil ma'lumot.",
+  },
+  {
+    href: '/docs/react/common-questions',
+    title: 'Umumiy savollar va javoblar',
+    description: "React.memo, useMemo va useCallback kabi texnikalarni to'g'ri ishlatish haqida ma'lumot.",
+  },
+  {
+    href: '/docs/react/powerful-patterns',
+    title: "Kuchli pattern'lar",
+    description: "Mashhur va kuchli pattern'lar haqida ma'lumot va ularning ahamiyati haqida ma'lumot.",
+  },
+  {
+    href: '/docs/react/server-side-react',
+    title: 'Server tomonda React',
+    description: "React'ning server tomonda qanday ishlashi haqida batafsil ma'lumot.",
+  },
+  {
+    href: '/docs/react/concurrent-react',
+    title: 'React Concurrent',
+    description: "React'ning bir vaqtda ishlashlik texnikalari haqida batafsil ma'lumot.",
+  },
+  {
+    href: '/docs/react/frameworks',
+    title: 'React freymvorklari',
+    description: "Next.js va Remix kabi React freymvorklari haqida batafsil ma'lumot.",
+  },
+  {
+    href: '/docs/react/react-server-components',
+    title: 'React server komponentlari',
+    description: "React server komponentlari qanday ishlashi haqida batafsil ma'lumot.",
+  },
+  {
+    href: '/docs/react/react-alternatives',
+    title: 'React alternativlari',
+    description: "Vue, Angular, Svelte va Solid kabi React alternativlari va texnikasi haqida ma'lumot.",
+  },
+]
