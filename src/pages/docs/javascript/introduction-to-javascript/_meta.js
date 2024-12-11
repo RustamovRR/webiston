8fa75ea464a961cd5f12f5aeefe65597ffb44a12
@riskiddin.rustamov <riskiddin.rustamov@paynet.uz>
2024-12-11 11:17:09 +0500

    feat: complete '1. introduction to javascript' chapter

--- src/pages/docs/javascript/introduction-to-javascript/_meta.js
+++ src/pages/docs/javascript/introduction-to-javascript/_meta.js
@@ -1,3 +1,7 @@
 export default {
   'exploring-javascript': 'JavaScript’ni oʻrganish',
+  'hello-world': 'Salom dunyo',
+  'tour-of-javascript': "Javascript bo'ylab sayohat",
+  'character-frequency-histograms': 'Misol: Belgilar chastotasi gistogrammalari',
+  summary: 'Xulosa',
 }
