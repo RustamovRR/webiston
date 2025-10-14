42dedfc26126fe73d4e9b6792d7bb3d9126c8619
Risqiddin Rustamov <rrisqiddin@gmail.com>
2025-10-14 18:54:39 +0500

    feat: completed '3-evaluation-methodology' chapter

--- src/components/mdx/ContentMeta/ContentMeta.tsx
+++ src/components/mdx/ContentMeta/ContentMeta.tsx
@@ -8,7 +8,7 @@ interface ContentMetaProps {
 export default function ContentMeta({ updatedAt }: ContentMetaProps) {
   return (
     <div className="my-0 flex items-center justify-between border-b border-[#F2F2F7] py-5 max-sm:flex-col max-sm:gap-2 dark:border-[#151515]">
-      <Link
+      {/* <Link
         href={`https://github.com/RustamovRR/webiston/issues/new`}
         target="_blank"
         rel="noopener noreferrer"
@@ -18,7 +18,7 @@ export default function ContentMeta({ updatedAt }: ContentMetaProps) {
           Kontentni yaxshilash uchun Githubda muammo ochish
         </span>
         <ArrowUpRightIcon className="stroke-[1px] text-[#8D8D93] duration-200 group-hover:text-black dark:group-hover:text-white" />
-      </Link>
+      </Link> */}
 
       {/* temporary disabled */}
       {/* {updatedAt && (
